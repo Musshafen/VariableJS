b1701cf3b6270a0b588c3a7c124b498690a6cabe
Musshafen <kmusshafen@hotmail.com>
2023-08-03 19:32:13 -0400

    created object variable aboutMe

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ import './style.css'
 let numberOfCupCoffee = 0
 
 let fullName = 'Kelly Musshafen'
+
+const aboutMe = {
+  fullName: fullName,
+  luckyNumber: 12,
+  favoriteMovies: ['Aladdin, The Dark Knight, The Life of David Gale'],
+}
